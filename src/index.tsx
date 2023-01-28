d4ee8fd361113e7f9055d132ab1b0b3fcdd0f6b1
Mattia <59755795+matt185@users.noreply.github.com>
2023-01-28 17:23:28 +0900

    add global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import "./styles/GlobalStyles.css"
 import App from './App';
+import "@fontsource/poppins"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/700.css"
 
 
 const root = ReactDOM.createRoot(
